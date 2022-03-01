98b514e95daf7050d01e64c3d57a2ba4976faad1
Mark Glushko <m.glushko@wnfx.ru>
2022-03-01 11:17:03 +0300

    Добавил 2 Unit теста

---
+++ app/src/test/java/com/glushko/films/UseCaseRepositoryTest.kt
@@ -0,0 +1,60 @@
+package com.glushko.films
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.glushko.films.business_logic_layer.domain.AboutFilm
+import com.glushko.films.business_logic_layer.interactor.UseCaseRepository
+import com.glushko.films.data_layer.datasource.ApiService
+import com.glushko.films.data_layer.datasource.response.ResponseFilm
+import com.glushko.films.data_layer.repository.FilmsDao
+import io.reactivex.Single
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mockito
+
+class UseCaseRepositoryTest {
+    @get:Rule
+    val schedulers = RxImmediateSchedulerRule()
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    private lateinit var useCase: UseCaseRepository
+    private lateinit var liveData: MutableLiveData<ResponseFilm>
+
+    private fun sample(): List<AboutFilm>{
+        return listOf(
+            AboutFilm(id = 1, name = "Test 1", img = ".img", typeList = "list"),
+            AboutFilm(id = 1, name = "Test 2", img = ".img", typeList = "list"),
+            AboutFilm(id = 1, name = "Test 3", img = ".img", typeList = "list")
+        )
+    }
+
+    private fun emptySample() = emptyList<AboutFilm>()
+
+    @Test
+    fun searchFilmFromDBisTrue(){
+        val searchText = "Test"
+        liveData = MutableLiveData()
+        val dao = Mockito.mock(FilmsDao::class.java)
+        val api = Mockito.mock(ApiService::class.java)
+        Mockito.`when`(dao.searchFilm(searchText)).thenReturn(Single.just(sample()))
+        useCase = UseCaseRepository(api, dao)
+        useCase.searchFilm(searchText, liveData)
+        Assert.assertEquals(true, liveData.value?.films?.isNotEmpty())
+    }
+
+    @Test
+    fun searchFilmFromDBisFalse(){
+        val searchText = "Test"
+        liveData = MutableLiveData()
+        val dao = Mockito.mock(FilmsDao::class.java)
+        val api = Mockito.mock(ApiService::class.java)
+        Mockito.`when`(dao.searchFilm(searchText)).thenReturn(Single.just(emptySample()))
+        useCase = UseCaseRepository(api, dao)
+        useCase.searchFilm(searchText, liveData)
+        Assert.assertEquals(false, liveData.value?.films?.isNotEmpty())
+    }
+
+}
